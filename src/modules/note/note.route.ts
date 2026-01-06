b899d84009ed4659adf24992395802373fccc75c
Ankit Nayak <ankitnayak882@gmail.com>
2026-01-07 01:01:28 +0530

    Add note routing with CRUD endpoints and validation middleware

---
+++ src/modules/note/note.route.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import { validate } from "middlewares/validate.middleware";
+import { createNoteSchema, updateNoteSchema } from "./note.schema";
+import * as controller from "./note.controller";
+
+const router = Router();
+
+router.post("/", validate(createNoteSchema), controller.createNoteHandler);
+router.get("/", controller.getNotesHandler);
+router.get("/:id", controller.getNoteHandler);
+router.put("/:id", validate(updateNoteSchema), controller.updateNoteHandler);
+router.delete("/:id", controller.deleteNoteHandler);
+
+export default router;
