399c10e731d143246a6ee09423ecbdf6f7e88a77
Ankit Nayak <ankitnayak882@gmail.com>
2026-01-07 00:58:07 +0530

    Refactor note schema formatting and ensure consistent use of Zod object structure

--- src/modules/note/note.schema.ts
+++ src/modules/note/note.schema.ts
@@ -1,15 +1,15 @@
-import {z} from 'zod'
+import { z } from "zod";
 
 export const createNoteSchema = z.object({
-    body: z.object({
-        title: z.string().min(3),
-        content: z.string().min(5)
-    })
-})
+  body: z.object({
+    title: z.string().min(3),
+    content: z.string().min(5),
+  }),
+});
 
 export const updateNoteSchema = z.object({
-    body: z.object({
-        title: z.string().min(3).optional(),
-        content: z.string().min(5).optional()
-    })
-})
+  body: z.object({
+    title: z.string().min(3).optional(),
+    content: z.string().min(5).optional(),
+  }),
+});
