3a079a9cfaddcd4c3c888f7055f01c1ea95ff7c2
Ankit Nayak <ankitnayak882@gmail.com>
2026-01-07 13:34:47 +0530

    Add request logging middleware to enhance request tracking in the application

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,11 @@ import { errorHandler } from "./middlewares/error.middleware";
 import { notFound } from "./middlewares/notFound.middleware";
 import healthRouter from "./routes/health.route";
 import noteRouter from "./modules/note/note.route";
+import { requestLogger } from "./middlewares/logger.middleware";
 
 const app = express();
 
+app.use(requestLogger);
 app.use(express.json());
 app.use("/health", healthRouter);
 app.use("/note", noteRouter);
