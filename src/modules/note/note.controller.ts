a4d3a37284187cdef50aa0105e506ca93f7f97e6
Ankit Nayak <ankitnayak882@gmail.com>
2026-01-07 13:26:22 +0530

    Refactor note controller to use asyncHandler for improved asynchronous error handling in CRUD operations

--- src/modules/note/note.controller.ts
+++ src/modules/note/note.controller.ts
@@ -1,19 +1,20 @@
 import { Request, Response } from "express";
 import * as noteService from "./note.service";
 import { apiResponse } from "../../utils/apiResponse";
-import { ApiError } from "utils/apiError";
+import { ApiError } from "../../utils/apiError";
+import { asyncHandler } from "../../utils/asyncHandler";
 
-export const createNoteHandler = (req: Request, res: Response) => {
+export const createNoteHandler = asyncHandler((req: Request, res: Response) => {
   const { title, content } = req.body;
   const note = noteService.createNote(title, content);
   res.status(201).json(apiResponse(note, "Note Created"));
-};
+});
 
-export const getNotesHandler = (_: Request, res: Response) => {
+export const getNotesHandler = asyncHandler((_: Request, res: Response) => {
   res.json(apiResponse(noteService.getAllNotes()));
-};
+});
 
-export const getNoteHandler = (req: Request, res: Response) => {
+export const getNoteHandler = asyncHandler((req: Request, res: Response) => {
   const id = req.params.id;
   if (!id) throw new ApiError(400, "Please send Note Id in Parameters");
 
@@ -22,9 +23,9 @@ export const getNoteHandler = (req: Request, res: Response) => {
   if (!note) throw new ApiError(404, "Note not Found");
 
   res.json(apiResponse(note));
-};
+});
 
-export const updateNoteHandler = (req: Request, res: Response) => {
+export const updateNoteHandler = asyncHandler((req: Request, res: Response) => {
   const id = req.params.id;
   if (!id) throw new ApiError(400, "Please send Note Id in Parameters");
 
@@ -32,12 +33,12 @@ export const updateNoteHandler = (req: Request, res: Response) => {
 
   if (!note) throw new ApiError(404, "Note not Found");
   res.json(apiResponse(note));
-};
+});
 
-export const deleteNoteHandler = (req: Request, res: Response) => {
+export const deleteNoteHandler = asyncHandler((req: Request, res: Response) => {
   const id = req.params.id;
   if (!id) throw new ApiError(400, "Please send Note Id in Parameters");
   const deleted = noteService.deleteNote(id);
   if (!deleted) throw new ApiError(404, "Note not Found");
   res.status(204).send();
-};
+});
